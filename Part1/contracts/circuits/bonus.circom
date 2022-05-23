1492d907fcd34152b92a8b6b47e19defa2ccf58e
Sedat Taskiran <se_tas@yahoo.com>
2022-05-23 23:30:55 +0300

    Assignment3 done

--- Part1/contracts/circuits/bonus.circom
+++ Part1/contracts/circuits/bonus.circom
@@ -1,1 +1,19 @@
 // [bonus] implement an example game from part d
+pragma circom 2.0.0;
+
+// [assignment] implement a variation of mastermind from https://en.wikipedia.org/wiki/Mastermind_(board_game)#Variation as a circuit
+
+include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/bitify.circom";
+include "../../node_modules/circomlib/circuits/poseidon.circom";
+
+
+template GridSetup() {
+    // Public inputs
+    signal input grid[4][4];
+    signal input mines[3];
+    signal input clues;
+
+}
+
+component main {public } = GridSetup();
--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,50 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const chai = require("chai");
+const path = require("path");
+
+const wasm_tester = require("circom_tester").wasm;
+const buildPoseidon = require("circomlibjs").buildPoseidon;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+const assert = chai.assert;
+
+describe("Mastermind test", function () {
+    let poseidon;
+    let F;
+
+    this.timeout(100000000);
+
+    it("Should create a MastermindVariation circuit", async () => {
+        poseidon = await buildPoseidon();
+        F = poseidon.F;
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        const privSalt = Fr.e(-1)
+        const pubSolnHash = poseidon([privSalt, 1, 2, 3]);
+        const pubSolnObj = F.toObject(pubSolnHash); 
+
+        const INPUT = {
+            "pubGuessA": "1",
+            "pubGuessB": "2",
+            "pubGuessC": "3",
+            "pubNumHit": "3",
+            "pubNumBlow": "0",
+            "pubSolnHash": pubSolnObj,
+            "privSolnA": "1",
+            "privSolnB": "2",
+            "privSolnC": "3",
+            "privSalt": privSalt,           
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]),Fr.e(pubSolnObj)));
+    });
+});
